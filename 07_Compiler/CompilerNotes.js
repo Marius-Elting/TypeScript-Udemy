9a0257ef0e40bcc9150977e9619c46487fd5338d
Marius-Elting <Marius.Supercode@gmail.com>
2023-04-17 15:36:59 +0200

    finished Compiler Lecture

---
+++ 07_Compiler/CompilerNotes.js
@@ -0,0 +1,3 @@
+"use strict";
+// watching TS Files
+// 
---
+++ 07_Compiler/CompilerNotes.ts
@@ -0,0 +1,4 @@
+// watching TS Files
+// run "tsc"
+// or run "tsc --watch" it will automaticly compile to JS files
+// if tsc watch is running it will show the Errors in the console
