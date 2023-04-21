ba81ab9fa7112a33cce9b7e4de9b497d42806b02
Marius-Elting <Marius.Supercode@gmail.com>
2023-04-21 16:32:53 +0200

    removed Project from Repo

--- 11_Project_Frontend/src/components/CreateTaskForm/interfaces/ITextField.ts
+++
@@ -1,6 +0,0 @@
-import { IDisabled } from "./IDisabled";
-import React from "react";
-
-export interface ITextField extends IDisabled {
-    onChange?: (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void
-}
